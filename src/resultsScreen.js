b89de02067ac2095d03849c546c327a095ba5758
Liam Shaw <liamjosephshaw@gmail.com>
2023-05-23 22:23:29 +0100

    Proper demo

--- src/resultsScreen.js
+++ src/resultsScreen.js
@@ -2,8 +2,34 @@ import { shareResult } from "./sheardle";
 
 const modal = document.getElementById("resultsModal");
 
-// The below should also take in the gameState?
-// The idea being, we need to show if the user was correct or not
+// Getting the difference in seconds to the next day
+function getTimeToNextDay() {
+    const now = new Date();
+    const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1);
+    const diffInMilliSeconds = tomorrow - now;
+    return diffInMilliSeconds / 1000;
+}
+
+// Countdown timer
+function startTimer(duration, display) {
+    let timer = duration, hours, minutes, seconds;
+    
+    setInterval(function () {
+        hours = parseInt(timer / 3600, 10);
+        minutes = parseInt((timer % 3600) / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = hours + ":" + minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
 
 export function gameEnd(trackInfo) {
 
@@ -15,10 +41,12 @@ export function gameEnd(trackInfo) {
     document.querySelector(".album-art").src = trackInfo.album.images[0].url;
     document.querySelector(".song-preview").src = trackInfo.preview_url;
 
+    let countdownElement = document.querySelector(".countdown");
+    let timeToNextDay = getTimeToNextDay();
+    startTimer(timeToNextDay, countdownElement);
+
     console.log(shareResult());
 }
-
-
 
 document.querySelector(".share-results-btn").addEventListener('click', function() {
     console.log("Copied results to clipboard", '\n\n', shareResult());
@@ -26,3 +54,4 @@ document.querySelector(".share-results-btn").addEventListener('click', function(
     // Copy to clipboard
     navigator.clipboard.writeText(shareResult());
 });
+
--- src/trackSelection.js
+++ src/trackSelection.js
@@ -71,3 +71,37 @@ export function getCurrentDay() {
 export function getTodaysTrackID() {
     return trackSelection[getCurrentDay()];
 }
+
+// Getting the difference in seconds to the next day
+function getTimeToNextDay() {
+    const now = new Date();
+    const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1);
+    const diffInMilliSeconds = tomorrow - now;
+    return diffInMilliSeconds / 1000;
+}
+
+// Countdown timer
+function startTimer(duration, display) {
+    let timer = duration, hours, minutes, seconds;
+    setInterval(function () {
+        hours = parseInt(timer / 3600, 10);
+        minutes = parseInt((timer % 3600) / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = hours + ":" + minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    let countdownElement = document.querySelector(".countdown"); // set the correct selector
+    let timeToNextDay = getTimeToNextDay();
+    startTimer(timeToNextDay, countdownElement);
+};
